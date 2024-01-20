ab110b9b284a515246a8e163907c47d8c082b608
provodd <provod.dev@gmail.com>
2024-01-20 11:01:54 +0300

    конфиг

---
+++ config.example.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'chatbot_service' => [
+        'access_token' => 'vk1.a..',
+        //'open_ai_key' => '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',
+        'open_ai_key' => 'sk-fDs..',
+        //'access_token' => 'vk1.a.grYZIodEZFCCK9F6fzXPK4e4-MYxheKbV3dPgzJOwK9BQaCd9bpfHYOlwnTwA9jXjGGQ7ygyt-aTIFdGdhW44pglZ2qWSc6LSMZ3GnQqiMMK_poe_BfhMmD_lLXYFIJ2mIS9p_bl6A8dMCH7zI5EyLq-qyY-ahJrrstgTb87aRSJ25EszHKqpEwmurGnN13kTuoHmJJwJSpq0JIZUq_mig',
+    ],
+    'victorina_service' => [
+        'access_token' => 'vk1.a...',
+        //'access_token' => 'vk1.a.grYZIodEZFCCK9F6fzXPK4e4-MYxheKbV3dPgzJOwK9BQaCd9bpfHYOlwnTwA9jXjGGQ7ygyt-aTIFdGdhW44pglZ2qWSc6LSMZ3GnQqiMMK_poe_BfhMmD_lLXYFIJ2mIS9p_bl6A8dMCH7zI5EyLq-qyY-ahJrrstgTb87aRSJ25EszHKqpEwmurGnN13kTuoHmJJwJSpq0JIZUq_mig',
+    ],
+    'post_service' => [
+        //'source_groups_id' => [-28627911, -47525482, -99834596, -85100484],
+        //906090 отсюда видосы можно брать
+        'source_groups_id' => [-123456, -123456, -123456],
+        'group_id' => -123456,
+        'access_token' => 'vk1.a.F...',
+    ],
+    'moderation_service' => [
+        'access_token' => 'vk1.a.G...',
+    ],
+    'mysql' => [
+        'host' => '127.0.0.1',
+        'user' => '',
+        'password' => '',
+        'database' => '',
+    ],
+
+];
